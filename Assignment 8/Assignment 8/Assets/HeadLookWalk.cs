d4ec9f5a10b02e066f445b0bfddb7f54eba39fd5
Jack Peterson <jackhp95@gmail.com>
2017-11-01 12:47:34 -0500

    Assignment 8

---
+++ Assignment 8/Assignment 8/Assets/HeadLookWalk.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+public class HeadLookWalk : MonoBehaviour
+{
+    public float velocity = 30.7f;
+    private CharacterController controller;
+
+	public bool isWalking = false;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+        if (Clicked())
+        {
+            isWalking = !isWalking;
+        }
+        if (isWalking)
+        {
+            controller.SimpleMove(Camera.main.transform.forward * velocity);
+        }
+    }
+    public bool Clicked()
+    {
+        return Input.anyKeyDown;
+    }
+}
