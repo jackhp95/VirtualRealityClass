89f849b916ddce4e4b34f2469c07a452b4009c62
Jack Peterson <jackhp95@gmail.com>
2017-11-08 11:22:01 -0600

    FPE

---
+++ First Person Experience/First Person Experience/Assets/DestroySelf.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySelf : MonoBehaviour
+{
+
+    public float endHeight = -5f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < endHeight)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
