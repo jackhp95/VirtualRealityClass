4ca03fb6c6b76f47fe1192a0d982cd037fee6d33
Jack Peterson <jackhp95@gmail.com>
2017-12-11 01:48:37 -0600

    final

---
+++ FinalProject/ThePool/Assets/CameraPointer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+
+public class CameraPointer : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Ray ray = this.GetComponent<Camera>().ScreenPointToRay(new Vector3(200, 200, 0));
+        Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+    }
+}
